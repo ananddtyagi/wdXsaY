b5af12733b1453e4ff24482776df8352fbb15221
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-09-30 15:53:40 -0700

    insight pipeline returns insights with sources

--- insight_pipeline.py
+++ insight_pipeline.py
@@ -10,6 +10,7 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
 import os
 
@@ -21,29 +22,34 @@ import os
 4. Return generated answer
 """
 
-def getDocuments(sourceName=""):
-    loader = PyPDFLoader("./api/sources/cleancode.pdf")
-    data = loader.load_and_split(RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100))
-    return data
 
 def getInsights():
 
-    documents = getDocuments()
+    vectorstore = FAISS.load_local("./faiss_index_clean_code", embeddings=OpenAIEmbeddings())
 
-    vectorstore = FAISS.from_documents(documents, embedding=OpenAIEmbeddings())
     retriever = vectorstore.as_retriever()
-
-    template = """
-    Answer the question based only on the following context. If you know know, don't return an answer, just say 'I don't know'
-    {context}
-
-    {question}
+    template = \
     """
+    Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
+    If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+    ALWAYS return a "SOURCES" part in your answer.
+    
+    QUESTION: {question}
+    =========
+    {summaries}
+    =========
+    FINAL ANSWER : 
+    SOURCES: """  
+    prompt_template = PromptTemplate(template=template, input_variables=["summaries", "question"])
+    chain_type_kwargs = {"prompt": prompt_template}
 
     chain = RetrievalQAWithSourcesChain.from_chain_type(
         llm=OpenAI(temperature=0),
-        retriever=retriever
+        retriever=retriever,
+        chain_type_kwargs=chain_type_kwargs,
+        return_source_documents=True
     )
+
 
     answer = chain({"question": "What does clean code say about writing comments?"})    
 
