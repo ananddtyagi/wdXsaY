89eabc5a556808af24915d3fe35d90297d98b61e
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-09-30 17:05:26 -0700

    get the hello world api response working

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 export const dynamicParams = true
 
-import { useState } from "react";
+import { useState } from 'react'
 import { useRouter, useSearchParams, usePathname } from 'next/navigation'
+import { FormEvent } from 'react';
 
 export default function Home() {
   const searchParams = useSearchParams()
   const params = new URLSearchParams(searchParams)
   const pathname = usePathname()
   const router = useRouter()
-
+  const [insights, setInsights] = useState("")
 
   const setSource = (newSource: string) => {
     console.log(newSource)
@@ -18,10 +19,29 @@ export default function Home() {
     router.push(pathname + "?" + params.toString())
   }
 
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      const response  = await fetch('api/getInsights', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const result = await response.json();
+
+      setInsights(result.message)
+      console.log(result)
+
+      
+  }
+
+
   return(
     <main>
       <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <form>
+        <form
+            onSubmit={onSubmit}
+        >
             What did
           <select
             id="source"
@@ -49,6 +69,9 @@ export default function Home() {
           >	
           </input>
         </form>
+        <div>
+          {insights}
+        </div>
       </div>
     </main>
   )
