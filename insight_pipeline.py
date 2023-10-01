8ff5dab6c2008c4082268f86820f2581171cea7a
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-10-01 11:11:50 -0700

    nit

--- insight_pipeline.py
+++ insight_pipeline.py
@@ -22,7 +22,7 @@ def generateInsights(question=""):
 
     retriever = vectorstore.as_retriever()
     template = \
-        """
+    """
     Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
     If you don't know the answer, just say that you don't know. Don't try to make up an answer.
     ALWAYS return a "SOURCES" part in your answer.
