f09a80c2d2d713b033715530237116d2516f3b06
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-09-30 23:13:24 -0700

    basic website created with connection to backend

--- app/page.tsx
+++ app/page.tsx
@@ -10,26 +10,32 @@ export default function Home() {
   const params = new URLSearchParams(searchParams)
   const pathname = usePathname()
   const router = useRouter()
+  const [source, setSource] = useState(searchParams.get("source") ?? "")
+  const [question, setQuestion] = useState(searchParams.get("question") ?? "")
   const [insights, setInsights] = useState("")
 
-  const setSource = (newSource: string) => {
-    console.log(newSource)
-    params.set("source", newSource)
-
-    router.push(pathname + "?" + params.toString())
+  const setSourceParam = (newSource: string) => {
+    setSource(newSource)
   }
 
+  const setQuestionParam = (newQuestion: string) => {
+    setQuestion(newQuestion)
+  };
+
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+
+      params.set("source", source)
+      params.set("question", question)
+
       event.preventDefault();
-      const formData = new FormData(event.currentTarget);
-      const response  = await fetch('api/getInsights', {
-        method: 'POST',
-        body: formData,
+      const response  = await fetch(`api/getInsights/?${params.toString()}`, {
+        method: 'GET',
       });
 
       const result = await response.json();
 
       setInsights(result.message)
+      router.push(pathname + "?" + params.toString())
       console.log(result)
 
       
@@ -46,8 +52,8 @@ export default function Home() {
           <select
             id="source"
             name="source"
-            value={searchParams.get("source") ?? ""}
-            onChange={(e) => setSource(e.target.value)}
+            value={source}
+            onChange={(e) => setSourceParam(e.target.value)}
             className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-10 mx-5 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
           >
             <option value="" disabled hidden>Select an option...</option>
@@ -57,9 +63,11 @@ export default function Home() {
           say about
           <input
             type="text"
-            name="query"
+            name="question"
             className="rounded-md border-0 py-1.5 pl-3 pr-10 mx-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            id="query"
+            id="question"
+            value={question}
+            onChange={(e) => setQuestionParam(e.target.value)}
           >
           </input>
           <input 
