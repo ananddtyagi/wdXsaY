1e2d676cc9cffd2831ba92f3a7e9c48949545ce4
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-10-02 23:40:31 -0700

    update frontend

--- app/(routes)/aboutme/page.tsx
+++ app/(routes)/aboutme/page.tsx
@@ -5,7 +5,6 @@ export default function AboutMe() {
             <h1>About Me</h1>
             <p>This website was made by Anand Tyagi. 
                 You can find out more about him <a href="https://ananddtyagi.me">here</a></p>
-            
         </div>
     )
 }
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 export const dynamicParams = true
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter, useSearchParams, usePathname } from 'next/navigation'
 import { FormEvent } from 'react';
 
@@ -15,13 +15,8 @@ export default function Home() {
   const [answer, setAnswer] = useState("")
   const [citations, setCitations] = useState([])
 
-  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-
-      params.set("source", source)
-      params.set("query", query)
-
-      event.preventDefault();
-      const response  = await fetch(`api/getInsights/?${params.toString()}`, {
+  const getAnswer = async () => {
+    const response = await fetch(`api/getInsights/?${params.toString()}`, {
         method: 'GET',
       });
 
@@ -31,68 +26,78 @@ export default function Home() {
       router.push(pathname + "?" + params.toString())
   }
 
+  useEffect(() => {
+    getAnswer()
+  }, []);
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+
+      params.set("source", source)
+      params.set("query", query)
+
+      getAnswer()
+  }
+
 
   return(
-    <>
-      <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <form
-            onSubmit={onSubmit}
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <form
+          onSubmit={onSubmit}
+      >
+          What does
+        <select
+          id="source"
+          name="source"
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+          className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-10 mx-5 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
         >
-            What does
-          <select
-            id="source"
-            name="source"
-            value={source}
-            onChange={(e) => setSource(e.target.value)}
-            className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-10 mx-5 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-          >
-            <option value="" disabled hidden>Select an option...</option>
-            <option value="Clean Code">Clean Code</option>
+          <option value="" disabled hidden>Select an option...</option>
+          <option value="Clean Code">Clean Code</option>
 
-          </select>          
-          say about
-          <input
-            type="text"
-            name="query"
-            className="rounded-md border-0 py-1.5 pl-3 pr-10 mx-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            id="query"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          >
-          </input>
-          <input 
-            type="submit" 
-            value="Let's find out!"
-            className="rounded-md ring-1 ring-inset ring-gray-300 px-5 py-1 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
-          >	
-          </input>
-        </form>
-        {answer && 
-          <div className="m-5">
+        </select>          
+        say about
+        <input
+          type="text"
+          name="query"
+          className="rounded-md border-0 py-1.5 pl-3 pr-10 mx-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          id="query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        >
+        </input>
+        <input 
+          type="submit" 
+          value="Let's find out!"
+          className="rounded-md ring-1 ring-inset ring-gray-300 px-5 py-1 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
+        >	
+        </input>
+      </form>
+      {answer && 
+        <div className="box m-5">
 
-            <div>
-              <h1>Answer</h1>
-              <p className="py-5 sm:text-sm">{answer}</p>
+          <div className="border rounded border-blue-500 p-5 shadow-lg m-5">
+            <h1>Answer</h1>
+            <p className="py-5 sm:text-sm">{answer}</p>
 
-            </div>
-            <div className="py-2">
-              <h1>Citations</h1>
-              <div className="py-5">
-              {citations.map((citation) => {
-                return (
-                  <div className="py-4">
-                    <p>Page: {citation.metadata.page}</p>
-                    <p>{citation.page_content}</p>
-                  </div>
+          </div>
+          <div className="py-2 border rounded border-green-500 p-5 m-5">
+            <h1>Citations</h1>
+            <div className="py-5">
+            {citations.map((citation) => {
+              return (
+                <div className="py-4">
+                  <p>Page: {citation.metadata.page}</p>
+                  <p>{citation.page_content}</p>
+                </div>
 
-                )
-              })}
-              </div>
+              )
+            })}
             </div>
           </div>
-        }
+        </div>
+      }
 
-      </div>
-    </>
+    </div>
   )
 }
