d7e86b2b1440bfc9c1e828a645fc0a9428677e11
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-10-07 01:36:59 -0700

    got the local dockerized version of the app working

--- api/index.py
+++ api/index.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from insight_pipeline import generateInsights, generateQuestion
 
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/getInsights")
 async def getInsights(source: str, query: str):
