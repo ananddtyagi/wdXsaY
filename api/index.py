4b6f759ff1038a8f0082ac578ce6ba9474fbfb71
Justin Chen <justinchen1000@gmail.com>
2023-10-31 20:55:04 -0400

    add environment var for deployment. Slightly improve UI (#15)

--- api/index.py
+++ api/index.py
@@ -1,32 +1,35 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import storage
+from firebase_admin import credentials
+from dotenv import load_dotenv
 
 import time
+import os
 
 from insight_pipeline import generateInsightsCustom, generateQuestion
 from generate_vectorstore import generateVectorStores
+from gfirebase import get_firebase_creds_dict
 
-BUCKET = "staging.wdxsay.appspot.com"
-
+load_dotenv()
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    try:
-        cred = credentials.Certificate("wdxsay-firebase-adminsdk-trxco-f704fd7e90.json")
-        firebase_admin.initialize_app(cred, {
-            'storageBucket': BUCKET
-        })
-    except Exception as err:
-        print("ERROR: Failed to initialize firebase authentication:", err)
+    load_dotenv()
+    cred_dict = get_firebase_creds_dict()
+    cred = credentials.Certificate(cred_dict)
 
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': os.environ["G_FIREBASE_BUCKET_URL"]
+    })
+
+# https://wdxsay.vercel.app
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://wdxsay.vercel.app"],  # Replace with your frontend URL(s)
+    allow_origins=["http://localhost:3000", os.environ["FRONTEND_ALLOWED_ORIGIN"]],  # Replace with your frontend URL(s)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -46,14 +49,14 @@ async def createUploadFile(file: UploadFile):
         contents = await file.read()
         tmp_file.write(contents)
 
-    vsStart = time.perf_counter()
+    vs_start = time.perf_counter()
     vs_bytes = await generateVectorStores(file_path)
-    vsEnd = time.perf_counter()
+    vs_end = time.perf_counter()
 
-    print(f"Generate Vectorstore Took {vsEnd - vsStart:0.4f} seconds")
+    print(f"Generate Vectorstore For {file.filename} Took {vs_end - vs_start:0.4f} seconds")
 
     bucket = storage.bucket()
-    fileStart = time.perf_counter()
+    file_start = time.perf_counter()
     
     # TODO: Fix bug where if .pdf doesn't exist in string.
     vectorstore_filename = file.filename.rsplit(".pdf")[0]
@@ -61,11 +64,8 @@ async def createUploadFile(file: UploadFile):
 
     # https://cloud.google.com/python/docs/reference/storage/latest/google.cloud.storage.blob.Blob?authuser=0#google_cloud_storage_blob_Blob_upload_from_string
     blob.upload_from_string(vs_bytes, content_type="application/octet-stream")
-    fileEnd = time.perf_counter()
+    file_end = time.perf_counter()
 
-    print(f"Uploading File Took {fileEnd - fileStart:0.4f} seconds")
+    print(f"Uploading File Took {file_end - file_start:0.4f} seconds")
 
     return {"filename": vectorstore_filename}
-
-if __name__ == "__main__":
-    app.run()
